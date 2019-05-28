27494baae642d23ffc8f4798e64419d6c5746736
Agata Miszkurka <ag.miszkurka@gmail.com>
2019-05-28 22:57:09 +0200

    tests added

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -6,9 +6,24 @@ def test_get_page():
     assert get_page('not-known') is None
 
 
-def test_app():
+def test_welcome_page():
     with app.test_client() as client:
         welcome_page = client.get('/')
         assert welcome_page.status_code == 200
-        assert b'inheritance' in welcome_page.data
-        assert b'Welcome' in welcome_page.data
+        assert b'navbar' in welcome_page.data, 'Not inheriting from base.html'
+        assert b'Welcome' in welcome_page.data, 'Welcome template not used'
+
+
+def test_redirection():
+    with app.test_client() as client:
+        requested_page = client.get('google')
+        assert requested_page.status_code == 302
+        assert requested_page.location == 'https://google.com'
+
+
+def test_404():
+    with app.test_client() as client:
+        requested_page = client.get('nothing')
+        assert requested_page.status_code == 404
+        assert b'navbar' in requested_page.data, 'Not inheriting from base.html'
+        assert b'Page not found' in requested_page.data, '404 template not used'
